39b4c7814f84f477ffa0bf3dc81b7d6314fc3453
liangzuning <1838169875@qq.com>
2025-12-02 09:46:10 +0000

    add git configs

--- startprofile.sh
+++ startprofile.sh
@@ -4,4 +4,6 @@ pip install transformers
 uv pip install libtpu
 huggingface-cli download inclusionAI/Ling-mini-2.0 --local-dir inclusionAI/Ling-mini-2.0
 
+git config --global user.email "1838169875@qq.com"
+git config --global user.name "liangzuning"
 git checkout ling_minimal
