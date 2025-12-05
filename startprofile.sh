81ba5c8616b219992719cc07b1064664bb7ca631
liangzuning <1838169875@qq.com>
2025-12-05 10:56:55 +0000

    add download scripts for ling minimal

--- startprofile.sh
+++ startprofile.sh
@@ -2,7 +2,14 @@ conda deactivate
 source ./.venv/bin/activate
 pip install transformers 
 uv pip install libtpu
-huggingface-cli download inclusionAI/Ling-mini-2.0 --local-dir inclusionAI/Ling-mini-2.0
+uv pip install jaxtypings
+chmod a+x ./hfd.sh
+
+alias hfd='./hfd.sh'
+
+sudo apt install aria2
+
+hfd inclusionAI/Ling-mini-2.0 --local-dir inclusionAI/Ling-mini-2.0
 
 git config --global user.email "1838169875@qq.com"
 git config --global user.name "liangzuning"
