fd692d66d87421ee803e5ce570ab763773a6d05d
liangzuning <1838169875@qq.com>
2025-12-09 05:12:00 +0000

    edit startprofile

--- startprofile.sh
+++ startprofile.sh
@@ -7,10 +7,13 @@ chmod a+x ./hfd.sh
 
 alias hfd='./hfd.sh'
 
-sudo apt install aria2
+sudo apt install aria2 -y
 
 hfd inclusionAI/Ling-mini-2.0 --local-dir inclusionAI/Ling-mini-2.0
 
 git config --global user.email "1838169875@qq.com"
 git config --global user.name "liangzuning"
 git checkout ling_minimal
+
+git clone https://github.com/AI-Hypercomputer/maxtext.git
+git clone https://github.com/jax-ml/bonsai.git
